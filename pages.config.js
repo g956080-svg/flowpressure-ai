57a83812b8f6bd2635304693c4fcd6939447ff16
g956080-svg <g956080@gmail.com>
2025-11-04 20:44:23 +0800

    Add files via upload

---
+++ pages.config.js
@@ -0,0 +1,59 @@
+import Dashboard from './pages/Dashboard';
+import StockDetail from './pages/StockDetail';
+import MoneyFlowRadar from './pages/MoneyFlowRadar';
+import AlertCenter from './pages/AlertCenter';
+import SafetyCoach from './pages/SafetyCoach';
+import VirtualPortfolio from './pages/VirtualPortfolio';
+import AutoTradeAI from './pages/AutoTradeAI';
+import RealTimeTrading from './pages/RealTimeTrading';
+import TestTrading from './pages/TestTrading';
+import BigMoneyRadar from './pages/BigMoneyRadar';
+import AILearningDashboard from './pages/AILearningDashboard';
+import WeeklyPerformance from './pages/WeeklyPerformance';
+import PerformanceReport from './pages/PerformanceReport';
+import SentimentRadar from './pages/SentimentRadar';
+import PulseHealth from './pages/PulseHealth';
+import ManualTrading from './pages/ManualTrading';
+import FlowReport from './pages/FlowReport';
+import AIStrategyConfig from './pages/AIStrategyConfig';
+import StrategyBacktest from './pages/StrategyBacktest';
+import StockPressureIndex from './pages/StockPressureIndex';
+import OpportunityScanner from './pages/OpportunityScanner';
+import TradingConsole from './pages/TradingConsole';
+import RadarHub from './pages/RadarHub';
+import ReportsHub from './pages/ReportsHub';
+import Layout from './Layout.jsx';
+
+
+export const PAGES = {
+    "Dashboard": Dashboard,
+    "StockDetail": StockDetail,
+    "MoneyFlowRadar": MoneyFlowRadar,
+    "AlertCenter": AlertCenter,
+    "SafetyCoach": SafetyCoach,
+    "VirtualPortfolio": VirtualPortfolio,
+    "AutoTradeAI": AutoTradeAI,
+    "RealTimeTrading": RealTimeTrading,
+    "TestTrading": TestTrading,
+    "BigMoneyRadar": BigMoneyRadar,
+    "AILearningDashboard": AILearningDashboard,
+    "WeeklyPerformance": WeeklyPerformance,
+    "PerformanceReport": PerformanceReport,
+    "SentimentRadar": SentimentRadar,
+    "PulseHealth": PulseHealth,
+    "ManualTrading": ManualTrading,
+    "FlowReport": FlowReport,
+    "AIStrategyConfig": AIStrategyConfig,
+    "StrategyBacktest": StrategyBacktest,
+    "StockPressureIndex": StockPressureIndex,
+    "OpportunityScanner": OpportunityScanner,
+    "TradingConsole": TradingConsole,
+    "RadarHub": RadarHub,
+    "ReportsHub": ReportsHub,
+}
+
+export const pagesConfig = {
+    mainPage: "Dashboard",
+    Pages: PAGES,
+    Layout: Layout,
+};
